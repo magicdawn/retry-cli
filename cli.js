31d54799e362e053c8a124f9f7c4ef1de7c9a324
magicdawn <magicdawn@qq.com>
2024-07-31 00:07:27 +0800

    fix: exit code

--- cli.js
+++ cli.js
@@ -1,34 +1,42 @@
 #!/usr/bin/env node
-import retry from 'retry';
-import spawn from 'cross-spawn';
-import Getopt from 'node-getopt';
+import retry from 'retry'
+import spawn from 'cross-spawn'
+import Getopt from 'node-getopt'
 
 const getopt = new Getopt([
   ['n', 'retries=ARG', 'Maximum amount of times to retry the operation. (default: 10)'],
   ['', 'factor=ARG', 'Exponential factor to use. (default: 2)'],
-  ['t', 'min-timeout=ARG', 'Number of milliseconds before starting the first retry. (default: 1000)'],
-  ['', 'max-timeout=ARG', 'Maximum number of milliseconds between two retries. (default: Infinity)'],
+  [
+    't',
+    'min-timeout=ARG',
+    'Number of milliseconds before starting the first retry. (default: 1000)',
+  ],
+  [
+    '',
+    'max-timeout=ARG',
+    'Maximum number of milliseconds between two retries. (default: Infinity)',
+  ],
   ['', 'randomize', 'Randomizes the timeouts by multiplying with a factor between 1 to 2.'],
-  ['h', 'help', 'Display this help.']
-]);
+  ['h', 'help', 'Display this help.'],
+])
 
 getopt.setHelp(
-  "Usage: retry [OPTION] -- [COMMAND]\n" +
-  "\n" +
-  "[[OPTIONS]]\n" +
-  "\n" +
-  "Examples:\n" +
-  "retry -- ls -lah dir\n" +
-  "retry -n 3 -t 100 -- ls asdf"
-);
+  'Usage: retry [OPTION] -- [COMMAND]\n' +
+    '\n' +
+    '[[OPTIONS]]\n' +
+    '\n' +
+    'Examples:\n' +
+    'retry -- ls -lah dir\n' +
+    'retry -n 3 -t 100 -- ls asdf'
+)
 
-const opt = getopt.parse(process.argv);
+const opt = getopt.parse(process.argv)
 
-const cmd = opt.argv.slice(2);
+const cmd = opt.argv.slice(2)
 
 if (!cmd[0]) {
-  getopt.showHelp();
-  process.exit();
+  getopt.showHelp()
+  process.exit()
 }
 
 const operation = retry.operation({
@@ -36,25 +44,29 @@ const operation = retry.operation({
   factor: opt.options['factor'] || 2,
   minTimeout: opt.options['min-timeout'] || 1000,
   maxTimeout: opt.options['max-timeout'] || Infinity,
-  randomize: opt.options['randomize'] || false
-});
+  randomize: opt.options['randomize'] || false,
+})
 
 operation.attempt(function (currentAttempt) {
-  const ls = spawn(cmd[0], cmd.slice(1), {stdio: 'inherit'});
+  const ls = spawn(cmd[0], cmd.slice(1), { stdio: 'inherit' })
 
-  function retryOrExit(err) {
-    const retrying = operation.retry(err);
+  function retryOrExit(err, code) {
+    const retrying = operation.retry(err)
     if (!retrying) {
-      process.exit(err);
+      process.exit(code)
     }
   }
 
   ls.on('exit', (code, signal) => {
-    retryOrExit(code !== 0);
-  });
+    if (code === 0) {
+      retryOrExit(undefined, code)
+    } else {
+      retryOrExit(new Error(`none 0 exit code ${code}`), code)
+    }
+  })
 
   ls.on('error', (err) => {
-    retryOrExit(err);
-  });
-
-});
+    // 1 means general error
+    retryOrExit(err, 1)
+  })
+})
